100950ff31c2371c8367450a0b0c67a6a3a42561
ddang9390 <dnld1ng@yahoo.com>
2018-06-24 17:49:18 -0400

    Organized folder structure and made record and playback functionality

---
+++ Assets/Player/Scripts/MyReplay.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyReplay : MonoBehaviour {
+    private const int bufferFrames = 100;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Record();
+    }
+
+    private void Record()
+    {
+        rigidbody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+
+        keyFrames[frame] = new MyKeyFrame(Time.time, transform.position, transform.rotation);
+    }
+
+    private void PlayBack()
+    {
+        rigidbody.isKinematic = true;
+        int frame = Time.frameCount % bufferFrames;
+
+        transform.position = keyFrames[frame].pos;
+        transform.rotation = keyFrames[frame].rot;
+    }
+}
+
+public class MyKeyFrame
+{
+    public float time;
+    public Vector3 pos;
+    public Quaternion rot;
+
+    public MyKeyFrame(float time, Vector3 pos, Quaternion rot)
+    {
+        this.time = time;
+        this.pos = pos;
+        this.rot = rot;
+    }
+}
