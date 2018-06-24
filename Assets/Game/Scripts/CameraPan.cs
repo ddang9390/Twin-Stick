cccf4333a6d314942bc75a3ca98d04f2d2addc89
ddang9390 <dnld1ng@yahoo.com>
2018-06-24 18:49:04 -0400

    Right stick can control the camera

---
+++ Assets/Game/Scripts/CameraPan.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("H: " + Input.GetAxis("RHorizontal"));
+        Debug.Log("V: " + Input.GetAxis("RVertical"));
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(player.transform);
+    }
+}
